3a69ee6371c9e91b4bc196cd767ee4dad996bc32
Nobb <khj59105@gmail.com>
2025-12-13 17:44:47 +0900

    feat: 이메일로 유저정보 찾기 구현
    - User 클래스 소문자 → 대문자 수정
    
    - 이메일로 유저정보 찾는 기능 구현

--- dev-log-backend/src/user/user.entity.ts
+++ dev-log-backend/src/user/user.entity.ts
@@ -2,7 +2,7 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('users')
 //typeorm에게 이 클래스가 users 테이블과 매핑됨을 알림
-export class user {
+export class User {
 	@PrimaryGeneratedColumn() // 자동으로 1씩 증가하는 숫자형 기본키 (Primary Key)
 	id: number; // INSERT 시 자동으로 ID 할당
 	
---
+++ dev-log-backend/src/user/user.service.ts
@@ -0,0 +1,25 @@
+// user-service (db작업)
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService{
+	constructor(
+		@InjectRepository(User) // Repository: TypeORM에서 제공하는 DB 작업 헬퍼 클래스
+		private userRepository: Repository<User>,
+	){}
+
+	async findByEmail(email: string): Promise<User | null>{
+		return this.userRepository.findOne({ // SQL: SELECT * FROM users WHERE email = 'email' LIMIT 1
+			where: { email }
+		});
+	}
+
+	// async create(email: string, pwd: string, name: string): Promise<User>{
+	// 	// 새 유저 생성 (회원가입 처리)
+		
+	// }
+}
